1e230107aec85719faf8dac388177496cc2192e7
nitendragautam <nitendra.gautam12@gmail.com>
2024-01-17 20:56:38 -0600

    adding 1581 query

---
+++ Oracle/intrvw_prob_soln/lc/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,8 @@
+
+
+/*https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/?envType=study-plan-v2&envId=top-sql-50 */
+
+SELECT customer_id, count(*) as count_no_trans from Visits
+WHERE visit_id  NOT IN 
+(SELECT visit_id FROM Transactions)
+GROUP BY customer_id
